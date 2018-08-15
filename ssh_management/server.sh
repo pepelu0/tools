01ada139b2e95175039dea576d40b2df8274aaba
lubei <lubei@iqianjin.com>
2018-08-15 17:20:03 +0800

    FIX: fetch wrong server when ssh

--- ssh_management/server.sh
+++ ssh_management/server.sh
@@ -2,7 +2,9 @@
 basepath=$(cd `dirname $0`; pwd)
 file=$basepath"/servers.conf"
 index=1
-
+ips=()
+user_names=()
+passwords=()
 printf "%-4s %-16s %-20s %-20s\n" No IP User Remark
 for i in `awk '{print $1}' $file`
 
@@ -10,13 +12,16 @@ do
 if [[ ! $i =~ ^# ]];then
   j=`awk -v I="$i" '{if(I==$1)print $2}' $file`
   k=`awk -v I="$i" '{if(I==$1)print $4}' $file`
+  ips[$index]=$i
+  user_name[$index]=$j
+  passwords[$index]=`awk -v I="$i" '{if(I==$1)print $3}' $file`
   printf "%-4s %-16s %-20s %-20s\n" $index $i $j $k
   index=$[index+1]
 fi
 done
 
 read s
-address=`sed -n "$s,1p" $file | awk "{print $1}"`
-user=`sed -n "$s,1p" $file | awk "{print $2}"`
-password=`sed -n "$s,1p" $file | awk "{print $3}"`
+address=${ips[$s]}
+user=${user_name[$s]}
+password=${passwords[$s]}
 expect $basepath"/logon.exp" $address $user $password
