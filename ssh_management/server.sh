d58df7b60deb158a015722447245c35d67e9957f
Pepe Lu <ll999ff111@126.com>
2019-05-27 16:28:43 +0800

    ssh_management port config support

--- ssh_management/server.sh
+++ ssh_management/server.sh
@@ -5,6 +5,7 @@ index=1
 ips=()
 user_names=()
 passwords=()
+ports=()
 printf "%-4s %-16s %-20s %-20s\n" No IP User Remark
 for i in `awk '{print $1}' $file`
 
@@ -12,9 +13,14 @@ do
 if [[ ! $i =~ ^# ]];then
   j=`awk -v I="$i" '{if(I==$1)print $2}' $file`
   k=`awk -v I="$i" '{if(I==$1)print $4}' $file`
+  port=`awk -v I="$i" '{if(I==$1)print $5}' $file`
+  if [ -z "$port" ]; then
+    port=22
+  fi
   ips[$index]=$i
   user_name[$index]=$j
   passwords[$index]=`awk -v I="$i" '{if(I==$1)print $3}' $file`
+  ports[$index]=$port
   printf "%-4s %-16s %-20s %-20s\n" $index $i $j $k
   index=$[index+1]
 fi
@@ -24,4 +30,5 @@ read s
 address=${ips[$s]}
 user=${user_name[$s]}
 password=${passwords[$s]}
-expect $basepath"/logon.exp" $address $user $password
+port=${ports[$s]}
+expect $basepath"/logon.exp" $address $user $password $port
